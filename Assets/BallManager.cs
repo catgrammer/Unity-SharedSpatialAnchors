db415e648d49a971a1baa1aaa1e65b63c797e940
G3P <73323107+GP2P@users.noreply.github.com>
2023-10-24 13:08:24 -0700

    add ball, floor, and ball manager

---
+++ Assets/BallManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Oculus;
+
+public class BallManager : MonoBehaviour
+{
+    public float speed = 1.0f; // Speed of the ball
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -5)
+        {
+            transform.position = new Vector3(transform.position.x, 10, transform.position.z);
+        }
+
+        Vector2 thumbstickInput = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+
+        if (thumbstickInput != Vector2.zero)
+        {
+            Vector3 moveDirection = new Vector3(thumbstickInput.x, 0, thumbstickInput.y);
+            transform.position += moveDirection * speed * Time.deltaTime;
+        }
+    }
+}
